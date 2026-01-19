be65b94e98a6701911ca1f9beac6a324c4b4d895
Roy-O-Toy <roytoyboy@gmail.com>
2026-01-19 23:54:06 +0800

    Delete validate_resort_data.py

--- validate_resort_data.py
+++
@@ -1,8 +0,0 @@
-from validator import ResortValidator
-import sys
-
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: python validate_resort_data.py [datafile.json|csv]")
-    else:
-        ResortValidator.run_validation(sys.argv[1])
